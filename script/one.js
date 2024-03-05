e5041576fc88fb92989efb0ea78afb594cfe2484
DIKSHANT YADAV <dikshantyadav2023@gmail.com>
2024-03-05 20:16:06 +0530

    Initial commit

---
+++ script/one.js
@@ -0,0 +1,33 @@
+
+
+Shery.mouseFollower({
+    //Parameters are optional.
+    skew: true,
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration: .41,
+  });
+
+  Shery.makeMagnet(".magnet" /* Element to target.*/, {
+    //Parameters are optional.
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration: .51,
+
+  });
+
+  Shery.imageMasker(".mask-target" /* Element to target.*/, {
+    //Parameters are optional.
+    mouseFollower: true,
+    text: "Shery",
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration: 1,
+  });
+
+  Shery.textAnimate(".text_logo" /* Element to target.*/, {
+   
+    style: 1,
+    y: 10,
+    delay: 0.1,
+    duration: 1,
+    
+    
+  });
